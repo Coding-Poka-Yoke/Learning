b8270105d5d501e82a95209f50a442fb9239b785
Balamurugesh M <balamurugesh14@gmail.com>
2020-04-20 01:04:48 +0530

    Data Structure : Single Linked List

---
+++ JavaTestProject/src/main/java/com/learning/dataStructure/SingleLinkedListNode.java
@@ -0,0 +1,13 @@
+package com.learning.dataStructure;
+
+public class SingleLinkedListNode {
+
+	int data;
+	SingleLinkedListNode next;
+	
+	public SingleLinkedListNode(int data) {
+		this.data = data;
+		this.next = null;
+	}
+		
+}
