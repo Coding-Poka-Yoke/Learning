d2e503580a8d5192fcfda5f2814ad0774047f7be
Naveena Bharathi <naveenabharathiv@gmail.com>
2020-04-21 22:51:39 +0530

    Javascript-Basics-Program1

---
+++ Javascript/Basics/index.js
@@ -0,0 +1,18 @@
+console.log("This is my first Javascript Program")
+let name = 'Naveena';
+let age = 26;
+let isFemale = true;
+let firstName = 'Bharathi';
+let lastName = 'null';
+const votingAge =  18;
+let education = {
+    sslc : 'KSVSS',
+    higherSecondary: 'CSIEwart',
+    college: 'SKREnggCollege'
+}
+let myHobbies = ['bible','learning','drawing','cooking']
+function greet(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+greet('Techies');
