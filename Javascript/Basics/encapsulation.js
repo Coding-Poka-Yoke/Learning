f915bd54d69f2bf30070a5652694b3c310bfd33d
Naveena Bharathi <naveenabharathiv@gmail.com>
2020-04-22 15:18:19 +0530

    Object-oriented Programming in Javascript- Encapsulation

---
+++ Javascript/Basics/encapsulation.js
@@ -0,0 +1,23 @@
+//Procedural program
+let basicSalary = 30000;
+let overTime = 10;
+let rate = 20;
+
+function getWage(basicSalary, overTime, rate) {
+    return basicSalary + (overTime * rate);
+}
+
+
+//Object-Oriented program
+let person = {
+    basicSalary: 40000,
+    overTime: 20,
+    rate: 40,
+
+    newWage: function() {
+        return this.basicSalary + (this.overTime * this.rate);
+    }
+}
+
+console.log(getWage(basicSalary, overTime, rate));
+console.log(person.newWage());
